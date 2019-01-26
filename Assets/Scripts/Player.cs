26f10e143e9986d73b4cf02ba322d078cf8b5637
Randy Phillips <calibermengsk@gmail.com>
2019-01-25 22:26:24 -0600

    Basic player movement Basic camera rotation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public PlayerCamera cam;
+    public float speed = 3;
+
+    [HideInInspector]
+    public Vector3 moveDirection;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = Vector3.zero;
+        moveDirection.x = Input.GetAxis("Horizontal") * speed;
+        moveDirection.z = Input.GetAxis("Vertical")* speed;
+
+        rb.velocity += cam.transform.TransformDirection(moveDirection);
+    }
+}
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Player player;
+    public float distance, speed, normalHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+        Vector2 pPos = new Vector2(player.transform.position.x, player.transform.position.z);
+        Vector2 cPos = new Vector2(transform.position.x, transform.position.z);
+        //transform.LookAt(player.transform);
+        float offsetHeight = player.moveDirection.z;
+        //transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, player.transform.position.y + normalHeight - offsetHeight, transform.position.z), Time.deltaTime * speed);
+        
+        
+        //transform.position = player.transform.position - (transform.forward * distance);
+
+        transform.localEulerAngles = new Vector3(45-Input.GetAxis("Vertical")*10, transform.localEulerAngles.y, Input.GetAxis("Horizontal") * 10);
+        transform.position = player.transform.position - (Vector3.forward * distance) + new Vector3(0, normalHeight, 0);// * Input.GetAxis("Vertical") 
+    }
+}
