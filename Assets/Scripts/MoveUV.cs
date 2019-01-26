05f88468637d3e5cdd196e712d7c8c40061183e7
Randy Phillips <calibermengsk@gmail.com>
2019-01-26 14:27:44 -0600

    Updated level to have seperate mesh for water from main island Updated water to have UV offset to look like it's flowing.

---
+++ Assets/Scripts/MoveUV.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUV : MonoBehaviour
+{
+    public float xSpeed = 1, ySpeed = 1;
+    // Start is called before the first frame update
+
+    Vector2 offset;
+
+    Renderer rend;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset.x += xSpeed * Time.deltaTime;
+        offset.y += ySpeed * Time.deltaTime;
+        foreach(Material mat in rend.materials)
+        {
+            mat.SetTextureOffset("_MainTex", offset);
+        }
+    }
+}
