5d5f40e41d0ba0665271fd178a8342bee5afb286
Randy Phillips <calibermengsk@gmail.com>
2019-01-26 13:00:30 -0600

    Added Main Menu scene Added globals script Added options menu to main menu Added fx/music volume loading/saving

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Globals : MonoBehaviour
+{
+    public static float fxVolume = 1, musicVolume = 1;
+
+    public Slider fxSlider, musicSlider;
+
+    public static Globals instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        fxVolume = PlayerPrefs.GetFloat("fxVolume", 1);
+        fxSlider.value = fxVolume;
+        musicVolume = PlayerPrefs.GetFloat("musicVolume", 1);
+        musicSlider.value = musicVolume;
+    }
+
+
+    public void UpdateMusic(float inVol)
+    {
+        musicVolume = inVol;
+        PlayerPrefs.SetFloat("musicVolume", inVol);
+    }
+
+    public void UpdateFX(float inVol)
+    {
+        fxVolume = inVol;
+        PlayerPrefs.SetFloat("fxVolume", inVol);
+    }
+}
