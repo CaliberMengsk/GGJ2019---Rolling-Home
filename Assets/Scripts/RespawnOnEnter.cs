ec5f3303a291591f31398573b46616256eaaabe4
Randy Phillips <calibermengsk@gmail.com>
2019-01-27 11:33:17 -0600

    Fixed main menu bug on completing the game. Made water cause respawn on entry

---
+++ Assets/Scripts/RespawnOnEnter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnOnEnter : MonoBehaviour
+{
+    public Level level;
+    private void OnTriggerEnter(Collider other)
+    {
+        Respawn();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Respawn();
+    }
+
+    public void Respawn()
+    {
+        level.Respawn();
+    }
+}
