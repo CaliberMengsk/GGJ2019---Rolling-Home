5cf0cc7c3ee86cbbf40f8c05c1de3059048bc541
Clarvel <mattlruss@gmail.com>
2019-01-27 15:06:39 -0600

    added collider events scripts for randy

---
+++ Assets/Scripts/CollisionEvents.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable] public class CollisionEvent : UnityEvent<Collision> { }
+
+public class CollisionEvents : MonoBehaviour {
+	public CollisionEvent CollisionEnter = new CollisionEvent();
+	public CollisionEvent CollisionStay = new CollisionEvent();
+	public CollisionEvent CollisionExit = new CollisionEvent();
+
+	void OnCollisionEnter(Collision collision) { CollisionEnter.Invoke(collision); }
+	void OnCollisionStay(Collision collision) { CollisionStay.Invoke(collision); }
+	void OnCollisionExit(Collision collision) { CollisionExit.Invoke(collision); }
+}
---
+++ Assets/Scripts/TriggerEvents.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable] public class TriggerEvent : UnityEvent<Collider> { }
+
+public class TriggerEvents : MonoBehaviour {
+	public TriggerEvent TriggerEnter = new TriggerEvent();
+	public TriggerEvent TriggerStay = new TriggerEvent();
+	public TriggerEvent TriggerExit = new TriggerEvent();
+
+	void OnTriggerEnter(Collider collider) { TriggerEnter.Invoke(collider); }
+	void OnTriggerStay(Collider collider) { TriggerStay.Invoke(collider); }
+	void OnTriggerExit(Collider collider) { TriggerExit.Invoke(collider); }
+}
