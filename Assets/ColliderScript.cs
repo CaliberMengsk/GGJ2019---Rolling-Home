0437942618b7b3597dd2aa9d3352789c76249afc
Clarvel <mattlruss@gmail.com>
2019-01-27 13:31:25 -0600

    particles?

---
+++ Assets/ColliderScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderScript : MonoBehaviour
+{
+	[SerializeField]
+	[Range(0, 180)]
+	float wallFloorSeparationThreshold = 45f;
+	[SerializeField]
+	float collisionSpeedThreshold = 0.1f;
+	[SerializeField]
+	ParticleSystem groundParticles;
+
+	void OnCollisionEnter(Collision collision) {
+		ContactPoint[] cps = new ContactPoint[collision.contactCount];
+		collision.GetContacts(cps);
+		foreach(ContactPoint cp in cps){
+			float angle = Vector3.Angle(cp.point - transform.position, Vector3.down);
+			if(angle < wallFloorSeparationThreshold && collision.impulse.sqrMagnitude > collisionSpeedThreshold){
+				groundParticles.Play();
+			}
+		}
+	}
+}
